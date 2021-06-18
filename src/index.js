38183b7dd4ca858f40256277f2bb376dd3303486
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './is_piped_';
-export * from './piped_a_';
-export * from './piped_txt_';
-//# sourceMappingURL=src/index.js.map
--- src/piped_a_.js
+++
@@ -1,7 +0,0 @@
-import { piped_txt_ } from './piped_txt_';
-export async function piped_a_() {
-    const piped_txt = await piped_txt_();
-    return piped_txt == null ? piped_txt : piped_txt.split('\n');
-}
-export { piped_a_ as _a1__piped };
-//# sourceMappingURL=src/piped_a_.js.map
